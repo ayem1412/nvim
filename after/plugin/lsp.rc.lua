3e39c4d89c84368975679f46f0b199a7b26b13f4
Anas Youssef El Mahdad <elmahdad.anasyoussef@proton.me>
2026-02-05 14:38:45 +0100

    definition fix

--- after/plugin/lsp.rc.lua
+++ after/plugin/lsp.rc.lua
@@ -59,7 +59,7 @@ local keymap = vim.keymap
 local opts = { silent = true, remap = true }
 
 keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-keymap.set('n', 'gd', vim.lsp.buf.type_definition, opts)
+keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
 keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
 keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
