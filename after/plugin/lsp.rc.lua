5f9d87ac155206c14a6207bb14214d748c77b311
Anas Youssef El Mahdad <elmahdad.anasyoussef@proton.me>
2026-02-05 17:36:58 +0100

    jsp

--- after/plugin/lsp.rc.lua
+++ after/plugin/lsp.rc.lua
@@ -45,6 +45,14 @@ vim.lsp.config('vtsls', {
 			},
 		},
 	},
+	on_attach = function(client)
+		local existing_capabilities = client.server_capabilities
+		if vim.bo.filetype == 'vue' then
+			existing_capabilities.semanticTokensProvider.full = false
+		else
+			existing_capabilities.semanticTokensProvider.full = true
+		end
+	end,
 	filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
 })
 
