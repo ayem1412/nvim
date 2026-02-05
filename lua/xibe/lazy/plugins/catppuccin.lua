23ba1b7911f8dd8fb31a0cbfdf499f597054c05b
Anas Youssef El Mahdad <elmahdad.anasyoussef@proton.me>
2026-02-05 12:19:24 +0100

    lualine

--- lua/xibe/lazy/plugins/catppuccin.lua
+++ lua/xibe/lazy/plugins/catppuccin.lua
@@ -2,5 +2,10 @@ return {
 	'catppuccin/nvim',
 	name = 'catppuccin',
 	priority = 1000,
+	opts = {
+		integrations = {
+			mason = true,
+		},
+	},
 	init = function() vim.cmd.colorscheme 'catppuccin-mocha' end,
 }
