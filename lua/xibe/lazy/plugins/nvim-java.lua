253252347aa2f7a8df7315757db0e311d5eecad6
Anas Youssef El Mahdad <elmahdad.anasyoussef@proton.me>
2026-02-09 14:06:15 +0100

    fixed on_attach on jdtls

--- lua/xibe/lazy/plugins/nvim-java.lua
+++ lua/xibe/lazy/plugins/nvim-java.lua
@@ -1,5 +1,16 @@
+local on_attach_ok, on_attach = pcall(require, 'xibe.lsp.utils.on_attach')
+if not on_attach_ok then
+	return vim.notify('COULD NOT LOAD ON_ATTACH', vim.log.levels.ERROR, { title = 'ON_ATTACH' })
+end
+
 return {
 	'nvim-java/nvim-java',
 	opts = {},
-	init = function() vim.lsp.enable 'jdtls' end,
+	init = function()
+		vim.lsp.config('jdtls', {
+			on_attach = on_attach,
+		})
+
+		vim.lsp.enable 'jdtls'
+	end,
 }
